5e073e300fb4ecb21b21d6607af1e9882d64c3cb
PHPDOC CI Job <ci-job@maintaina.com>
2021-03-29 14:27:41 +0000

    Updated phpdoc for maintaina-com/Css_Parser (FRAMEWORK_6_0 branch) from ci

---
+++ FRAMEWORK_6_0/Css_Parser/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Horde_Css_Parser",
+            "name": "Horde_Css_Parser",
+            "summary": "Horde\u0020interface\u0020to\u0020the\u0020Sabberworm\u0020CSS\u0020Parser\u0020library.",
+            "url": "classes/Horde-Css-Parser.html"
+        },                {
+            "fqsen": "\\Horde_Css_Parser\u003A\u003A__construct\u0028\u0029",
+            "name": "__construct",
+            "summary": "Constructor.",
+            "url": "classes/Horde-Css-Parser.html#method___construct"
+        },                {
+            "fqsen": "\\Horde_Css_Parser\u003A\u003Acompress\u0028\u0029",
+            "name": "compress",
+            "summary": "Returns\u0020compressed\u0020CSS\u0020output.",
+            "url": "classes/Horde-Css-Parser.html#method_compress"
+        },                {
+            "fqsen": "\\Horde_Css_Parser\u003A\u003A\u0024doc",
+            "name": "doc",
+            "summary": "The\u0020parsed\u0020CSS\u0020document\u0020object.",
+            "url": "classes/Horde-Css-Parser.html#property_doc"
+        },                {
+            "fqsen": "\\Horde_Css_Parser\u003A\u003A\u0024parser",
+            "name": "parser",
+            "summary": "The\u0020parser\u0020object.",
+            "url": "classes/Horde-Css-Parser.html#property_parser"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
