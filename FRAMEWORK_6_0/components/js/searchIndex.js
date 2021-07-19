95b0475093dd8d1955e91c6337546e89015d1cb7
PHPDOC CI Job <ci-job@maintaina.com>
2021-07-19 09:03:56 +0000

    Updated phpdoc for maintaina-com/components (FRAMEWORK_6_0 branch) from ci

---
+++ FRAMEWORK_6_0/components/js/searchIndex.js
@@ -0,0 +1,34 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Components_Configs",
+            "name": "Components_Configs",
+            "summary": "",
+            "url": "classes/Components-Configs.html"
+        },                {
+            "fqsen": "\\Components_Dependencies_Injector",
+            "name": "Components_Dependencies_Injector",
+            "summary": "",
+            "url": "classes/Components-Dependencies-Injector.html"
+        },                {
+            "fqsen": "\\Components_Exception",
+            "name": "Components_Exception",
+            "summary": "",
+            "url": "classes/Components-Exception.html"
+        },                {
+            "fqsen": "\\Components",
+            "name": "Components",
+            "summary": "",
+            "url": "classes/Components.html"
+        },                {
+            "fqsen": "\\Components_Config_File",
+            "name": "Components_Config_File",
+            "summary": "",
+            "url": "classes/Components-Config-File.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
