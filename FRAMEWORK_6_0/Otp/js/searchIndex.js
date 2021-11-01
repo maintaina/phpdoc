f4aac5abb8a7e618c3bc4886ab3d868468c5f4d2
PHPDOC CI Job <ci-job@maintaina.com>
2021-11-01 08:09:48 +0000

    Updated phpdoc for maintaina-com/Otp (FRAMEWORK_6_0 branch) from ci

---
+++ FRAMEWORK_6_0/Otp/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Horde\\Otp\\Facade",
+            "name": "Facade",
+            "summary": "Facade\u0020for\u0020interacting\u0020with\u0020the\u0020OTP\u0020system",
+            "url": "classes/Horde-Otp-Facade.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Horde\\Otp",
+            "name": "Otp",
+            "summary": "",
+            "url": "namespaces/horde-otp.html"
+        },                {
+            "fqsen": "\\Horde",
+            "name": "Horde",
+            "summary": "",
+            "url": "namespaces/horde.html"
+        }            ]
+);
