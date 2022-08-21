44065473ff33dabfb87677e1602bcc593af2f74c
PHPDOC CI Job <ci-job@maintaina.com>
2022-08-21 06:31:00 +0000

    Updated phpdoc for maintaina-com/Registry (FRAMEWORK_6_0 branch) from ci

---
+++ FRAMEWORK_6_0/Registry/js/searchIndex.js
@@ -0,0 +1,24 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\Horde\\Registry\\Registry",
+            "name": "Registry",
+            "summary": "The\u0020Registry",
+            "url": "classes/Horde-Registry-Registry.html"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        },                {
+            "fqsen": "\\Horde\\Registry",
+            "name": "Registry",
+            "summary": "",
+            "url": "namespaces/horde-registry.html"
+        },                {
+            "fqsen": "\\Horde",
+            "name": "Horde",
+            "summary": "",
+            "url": "namespaces/horde.html"
+        }            ]
+);
